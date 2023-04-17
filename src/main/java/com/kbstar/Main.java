2108ce5238e9a56d60aca57e7a33ca2e8915006f
lssy2k1 <124110194+lssy2k1@users.noreply.github.com>
2023-04-17 15:42:07 +0900

    0417

--- src/main/java/com/kbstar/Main.java
+++ src/main/java/com/kbstar/Main.java
@@ -7,7 +7,8 @@ public class Main {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
 
-
+        int a = 10;
+        int b = 20;
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
